5b49ffb7b78468cc09733b6e08a189853f20a5f9
Kusal Kithul-Godage <git@kusal.io>
2023-04-23 22:25:42 +1000

    Rotate tease on load

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 let rotateDeg = 0;
 const card = document.getElementById('card');
 const flipIcons = document.querySelectorAll('.flip');
-flipIcons.forEach(icon => icon.addEventListener('click', rotate));
+flipIcons.forEach(icon => icon.addEventListener('click', rotate180));
+setTimeout(() => setRotate(30), 1);
+setTimeout(() => setRotate(0), 500);
 
-function rotate() {
-    rotateDeg += 180;
+function rotate180() {
+    setRotate(rotateDeg + 180);
+}
+
+function setRotate(deg) {
+    rotateDeg = deg;
     card.style.transform = `rotateY(${rotateDeg}deg)`;
 }
