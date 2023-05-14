c0530f91672584291866a27907407d66401d8064
Kusal Kithul-Godage <git@kusal.io>
2023-05-14 18:55:02 +1000

    Accessibility fixes

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 let rotateDeg = 0;
-const card = document.getElementById('card');
-const flipIcons = document.querySelectorAll('.flip');
-flipIcons.forEach(icon => icon.addEventListener('click', rotate180));
+const card = document.getElementById("card");
+const front = document.getElementById("front");
+const back = document.getElementById("back");
+const frontFlip = document.querySelector('#front .flip');
+const backFlip = document.querySelector('#back .flip');
+const flipIcons = [frontFlip, backFlip];
+flipIcons.forEach(icon => icon.addEventListener("click", e => clickFlip(e.target)));
+back.setAttribute("inert", "");
+
+// Tease spin on initial load
 setTimeout(() => setRotate(30), 1);
 setTimeout(() => setRotate(0), 500);
+
+function clickFlip(target) {
+    rotate180();
+    setFocusInert();
+    flipIcons.filter(i => i !== target)[0].focus();
+}
 
 function rotate180() {
     setRotate(rotateDeg + 180);
@@ -12,4 +25,15 @@ function rotate180() {
 function setRotate(deg) {
     rotateDeg = deg;
     card.style.transform = `rotateY(${rotateDeg}deg)`;
+}
+
+function setFocusInert() {
+    let focused = front;
+    let unfocused = back;
+    if (rotateDeg % 360) {
+        focused = back;
+        unfocused = front;
+    }
+    unfocused.setAttribute("inert", "");
+    focused.removeAttribute("inert");
 }
